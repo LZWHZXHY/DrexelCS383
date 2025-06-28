4fce55fc41fcfb76f004d2d5354d6f7db40641cf
LZWHZXHY <LZWHZXHY@gmail.com>
2025-06-28 15:37:52 -0400

    Finished Question 1

--- Homework1/main.py
+++ Homework1/main.py
@@ -1,25 +1,26 @@
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
-X = np.array([
-    [-2],
-    [-5],
-    [-3],
-    [0],
-    [-8],
-    [-2],
-    [1],
-    [5],
-    [-1],
-    [6]
-])
-
-Y = np.array([[1, -4,1,3,11,5,0,-1,-3,1]]).reshape(-1, 1)
+X = np.array([-2, -5, -3, 0, -8, -2, 1, 5, -1, 6]).reshape(-1, 1)
+Y = np.array([1, -4, 1, 3, 11, 5, 0, -1, -3, 1]).reshape(-1, 1)  # 直接reshape
 
 X_aug = np.hstack([np.ones((X.shape[0], 1)), X])
 
-print(X_aug)
-print(Y)
+#print(X_aug)
+#print(Y)
 
-theta = np.linalg.inv(X_aug.T @ X_aug) @ X_aug.T @ Y
+theta = np.linalg.inv(X_aug.T @ X_aug) @ (X_aug.T @ Y)
+print("Theory: ")
+print("Question 1 (a): ")
+print("Theta:\n", theta, "\n")
+print("Model Equation: y =", theta[0, 0], "+", theta[1, 0], "* x")
 
-print(theta)
+
+model = LinearRegression()
+model.fit(X, Y)
+
+print("Question 1  (b): ")
+print("Theta_0:", model.intercept_[0])
+print("Theta_1:", model.coef_[0])
+
+print("Question 2 (a): ")
